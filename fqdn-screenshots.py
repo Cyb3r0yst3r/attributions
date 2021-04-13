9059b3fa556eed60037d2b3fd398902e38b38fd5
Bayou Cyber Data Science <80774444+Cyb3r0yst3r@users.noreply.github.com>
2021-04-13 16:12:13 -0500

    Create fqdn-screenshots.py

---
+++ fqdn-screenshots.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+import os
+
+# resolve a text file containing a list of hostnames and take a screen shot
+
+fList = sys.argv[1]
+oDir = sys.argv[2]
+
+notResolvable = 0
+reSolvable = 0
+
+with open(fList, "r") as ins:
+   for line in ins:
+      hostName = line.strip()
+      hostName = hostName[:-1]   # Remove comma
+      
+      print ("HOSTNAME = [{}]" .format(hostName))
+      try:
+         ipAddr = socket.gethostbyname_ex(hostName)
+      except socket.error:
+         notResolvable += 1
+         print(f"Not resolvable = " + str(notResolvable))
+      else:
+         webSite = "curl \"https://website-screenshot.whoisxmlapi.com/api/v1?apiKey=<YOUR_API_KEY>&url=" + hostName + "&credits=DRS\" -o " + oDir + hostName + ".jpg"
+         os.system(webSite)
+         print(webSite)
+         reSolvable += 1
+         print(f"Resolvable = " + str(reSolvable))
